3737a0d337f2b343bfc72be32cac57a242131423
EoghanOConnor <eoghan.oconnor@intel.com>
2023-06-02 10:17:54 +0100

    Cleaning up outputs dir, adding copy of excel used

--- lighterFluid/lighterFluid.py
+++ lighterFluid/lighterFluid.py
@@ -4,15 +4,19 @@ from functions import importBuilder
 from functions import counterBuilder
 from functions import testBuilder
 from functions import dutflowBuilder
-from functions import flowFileBuilder
+from datetime import datetime
+import shutil
 
 
 
 #########################
 ##### CONFIGURATION #####
 #########################
-#definitionFile = r"C:\Users\adambyrn\OneDrive - Intel Corporation\ARRAY\lighterFluidFiles\lnlArrayMasterSheet.xlsx";
-definitionFile = r"C:\Users\lsuareza\OneDrive - Intel Corporation\ARRAY\lighterFluidFiles\lnlArrayMasterSheet.xlsx";
+#definitionDir = r"C:\Users\adambyrn\OneDrive - Intel Corporation\ARRAY\lighterFluidFiles";
+#definitionDir = r"C:\Users\lsuareza\OneDrive - Intel Corporation\ARRAY\lighterFluidFiles";
+definitionDir = r"C:\Users\eoghanoc\OneDrive - Intel Corporation\ARRAY\lighterFluidFiles";
+file = "lnlArrayMasterSheet.xlsx";
+
 definitionList = [];
 definitionList.append("arr_atom");
 definitionList.append("arr_ccf");
@@ -20,9 +24,24 @@ definitionList.append("arr_core");
 definitionList.append("arr_gfx");
 definitionList.append("arr_soc");
 definitionList.append("arr_vpu");
-findAndReplaceFile = "inputs\\findAndReplaceFile.csv";
+#findAndReplaceFile = "inputs\\findAndReplaceFile.csv";
 
+currSecond = str(datetime.now().second).zfill(2);
+currMinute = str(datetime.now().minute).zfill(2);
+currHour =   str(datetime.now().hour).zfill(2);
 
+currDay =   str(datetime.now().day).zfill(2);
+currMonth = str(datetime.now().month).zfill(2);
+currYear =  str(datetime.now().year).zfill(4);
+
+folderName = "_".join([currYear, currMonth, currDay, currHour, currMinute, currSecond]);
+
+outDir = "outputs\\" + folderName + "\\";
+fileToUse = outDir + "\\" + file;
+if not os.path.exists(outDir):
+    os.makedirs(outDir);
+
+shutil.copyfile(definitionDir + "\\" + file, fileToUse)
 
 #####################
 ##### EXECUTION #####
@@ -30,7 +49,15 @@ findAndReplaceFile = "inputs\\findAndReplaceFile.csv";
 for definitionPage in definitionList:
     ### read in File
     overallOutput = "";
-    dataset = pandas.read_excel(definitionFile, sheet_name=definitionPage);
+    currModule = definitionPage.upper();
+    
+    dataset = pandas.read_excel(fileToUse, sheet_name=definitionPage);
+    
+    moduleDir = outDir + currModule + "\\"; 
+    if not os.path.exists(moduleDir):
+        os.makedirs(moduleDir);
+    outputMtpl = outDir + currModule + "\\" + currModule + ".mtpl"; 
+
 
     importSection = importBuilder.importBuilder(dataset);
     counterSection = counterBuilder.counterBuilder(dataset);
@@ -41,7 +68,7 @@ for definitionPage in definitionList:
     
     currModule = definitionPage.upper();
 
-    moduleDir = "outputs\\" + currModule + "\\"; 
+    moduleDir = "outputs\\" + currModule + "\\";
     if not os.path.exists(moduleDir):
         os.makedirs(moduleDir);
     outputMtpl = "outputs\\" + currModule + "\\" + currModule + ".mtpl"; 
