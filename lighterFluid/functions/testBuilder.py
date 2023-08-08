51eb7f35f2a9f0023c40cd0282971951417d1c5d
EoghanOConnor <eoghan.oconnor@intel.com>
2023-08-08 11:35:44 +0100

    fixing StartIndex

--- lighterFluid/functions/testBuilder.py
+++ lighterFluid/functions/testBuilder.py
@@ -5,66 +5,6 @@ import math
 ####################
 ##### FUNCTION #####
 ####################
-def testBuilder(dataset,findAndReplaceFile, product):
-    # This function should build the test list.
-
-    ##################
-    ##### BASICS #####
-    ##################
-    testList = [];
-
-    findAndReplaceDict = {};
-    with open(findAndReplaceFile, 'r') as file:
-        for line in file:
-            splitLine = line.strip().split(",");
-            findAndReplaceDict[splitLine[0]] = splitLine[1];
-
-    for i in range(0,len(dataset.Template)):
-        # ignore composites
-        if(("COMPOSITE" in dataset.Template[i]) or ("TP_BEGIN" in dataset.Template[i]) or ("TP_END" in dataset.Template[i])):
-            continue;
-        
-        currTemplate = dataset.Template[i];
-
-        fileToOpen = "testTemplates\\" + product + "\\" + currTemplate + ".txt"
-
-        with open(fileToOpen, 'r') as file:
-            currTest = file.read();
-
-        for key,value in findAndReplaceDict.items():
-            if value in dataset.columns.values.tolist():
-                #print(dataset[value][i])
-                
-                # This section is for numbers
-                if (key in ["###baseNumber###","###bypassGlobal###"]):
-                    if math.isnan(dataset[value][i]):
-                        currValue = "";
-                    else:
-                        currValue = int(dataset[value][i]);
-                        currValue = str(currValue);
-                # This section is for strings
-                elif (key in ["###SetPointsPreInstance###",
-                              "###printToItuff###",
-                              "###DtsConfigName###",
-                              "###ifpmFile###",
-                              "###ifpmMod###",
-                              "###preinstance###",
-                              "###postinstance###"]):
-                    try:
-                        if math.isnan(dataset[value][i]):
-                            currValue = "";
-                    except:
-                        currValue = str(dataset[value][i]);
-                else:
-                    currValue = str(dataset[value][i]);
-
-                currTest = currTest.replace(key,currValue);
-
-        testList.append(currTest);
-
-    testListSection = "\r\n".join(testList);
-    return testListSection;
-
 def testBuilder(dataset, product):
     # This function should build the test list.
 
@@ -91,7 +31,7 @@ def testBuilder(dataset, product):
             currValue = "";
             
             # This section is for numbers
-            if (value in ["baseNumber","bypassGlobal"]):
+            if (value in ["baseNumber","bypassGlobal","StartIndex"]):
                 if math.isnan(dataset[value][i]):
                     currValue = "";
                 else:
